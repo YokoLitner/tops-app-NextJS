8bb4f55b0b2f30cd71d9aa05f7cbb75eacd8c6a4
Mark Chalenko <mark.chalenko@rndx2.com>
2022-08-25 19:56:29 +0300

    feat: grid-layout of header,footer,sudebar and body

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,20 +2,19 @@ import { LayoutProps } from './Layout.props';
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
 import { Sidebar } from './Sidebar/Sidebar';
+import styles from './Layout.module.css';
 import { FunctionComponent } from 'react';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>
-                    {children}
-                </div>
-            </div>
-            <Footer />
-        </>
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
+                {children}
+            </div>     
+            <Footer className={styles.footer}/>
+        </div>
     );
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,29 @@
-import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+    DocumentContext,
+    DocumentInitialProps,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
     }
     render(): JSX.Element {
         return (
-            <Html lang='ru'>
+            <Html lang="ru">
                 <Head>
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />
